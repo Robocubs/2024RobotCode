58c45fc63f0326f3ff0f791254b13b9f7a104e13
faeqk <s.filak26@student.uofdjesuit.orm>
2024-01-15 14:33:29 -0500

    Made Spark Flex file

---
+++ src/main/java/com/team1701/lib/drivers/motors/MotorIOSparkFlex.java
@@ -0,0 +1,103 @@
+package com.team1701.lib.drivers.motors;
+
+import java.util.Optional;
+import java.util.Queue;
+
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkFlex;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
+import com.team1701.lib.util.SignalSamplingThread;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
+
+public class MotorIOSparkFlex implements MotorIO {
+    private final CANSparkFlex mMotor;
+    private final RelativeEncoder mEncoder;
+    private final SparkPIDController mController;
+    private final double mReduction;
+
+    private Optional<Queue<Double>> mPositionSamples = Optional.empty();
+    private Optional<Queue<Double>> mVelocitySamples = Optional.empty();
+
+    public MotorIOSparkFlex(CANSparkFlex motor, double reduction) {
+        mMotor = motor;
+        mEncoder = motor.getEncoder();
+        mController = motor.getPIDController();
+        mReduction = reduction;
+    }
+
+    @Override
+    public void updateInputs(MotorInputs inputs) {
+        inputs.positionRadians = Units.rotationsToRadians(mEncoder.getPosition()) * mReduction;
+        inputs.velocityRadiansPerSecond =
+                Units.rotationsPerMinuteToRadiansPerSecond(mEncoder.getVelocity()) * mReduction;
+        mPositionSamples.ifPresent(samples -> {
+            inputs.positionRadiansSamples = samples.stream()
+                    .mapToDouble((position) -> Units.rotationsToRadians(position))
+                    .toArray();
+            samples.clear();
+        });
+        mVelocitySamples.ifPresent(samples -> {
+            inputs.velocityRadiansPerSecondSamples = samples.stream()
+                    .mapToDouble((velocity) -> Units.rotationsPerMinuteToRadiansPerSecond(velocity))
+                    .toArray();
+            samples.clear();
+        });
+    }
+
+    @Override
+    public void setPositionControl(Rotation2d position) {
+        mController.setReference(position.getRotations(), CANSparkFlex.ControlType.kPosition);
+    }
+
+    @Override
+    public void setVelocityControl(double velocityRadiansPerSecond) {
+        mController.setReference(
+                Units.radiansPerSecondToRotationsPerMinute(velocityRadiansPerSecond),
+                CANSparkFlex.ControlType.kVelocity);
+    }
+
+    @Override
+    public void setPercentOutput(double percentage) {
+        mController.setReference(percentage, CANSparkFlex.ControlType.kDutyCycle);
+    }
+
+    @Override
+    public void setVoltageOutput(double volts) {
+        mController.setReference(volts, CANSparkFlex.ControlType.kVoltage);
+    }
+
+    @Override
+    public void setBreakMode(boolean enable) {
+        mMotor.setIdleMode(enable ? IdleMode.kBrake : IdleMode.kCoast);
+    }
+
+    @Override
+    public void setPID(double ff, double p, double i, double d) {
+        mController.setFF(ff);
+        mController.setP(p);
+        mController.setI(i);
+        mController.setD(d);
+    }
+
+    @Override
+    public synchronized void enablePositionSampling(SignalSamplingThread samplingThread) {
+        if (mPositionSamples.isPresent()) {
+            throw new IllegalStateException("Position sampling already enabled");
+        }
+
+        var queue = samplingThread.addSignal(() -> Units.rotationsToRadians(mEncoder.getPosition()));
+        mPositionSamples = Optional.of(queue);
+    }
+
+    @Override
+    public synchronized void enableVelocitySampling(SignalSamplingThread samplingThread) {
+        if (mVelocitySamples.isPresent()) {
+            throw new IllegalStateException("Velocity sampling already enabled");
+        }
+
+        var queue = samplingThread.addSignal(() -> Units.rotationsPerMinuteToRadiansPerSecond(mEncoder.getVelocity()));
+        mVelocitySamples = Optional.of(queue);
+    }
+}
