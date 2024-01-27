155aa78b1bea256f90066297c0d912166584c72c
Marc Pulte <marcpulte@gmail.com>
2024-01-27 14:01:52 -0500

    Fixed initial loading of sendable chooser

--- src/main/ts/dashboard/src/dashboard.ts
+++ src/main/ts/dashboard/src/dashboard.ts
@@ -1,7 +1,8 @@
 import { consume } from '@lit/context';
 import { LitElement, css, html } from 'lit';
-import { customElement, state } from 'lit/decorators.js';
+import { customElement } from 'lit/decorators.js';
 import { NetworkTables, nt4Context } from './network-tables/network-tables';
+import { ensureSendableChooserInitialized } from './network-tables/util';
 
 @customElement('team1701-dashboard')
 export class Dashboard extends LitElement {
@@ -12,13 +13,16 @@ export class Dashboard extends LitElement {
     return this;
   }
 
+  protected firstUpdated(): void {
+    ensureSendableChooserInitialized(this.nt, '/SmartDashboard/Auto Mode');
+  }
+
   render() {
     return html`
       <div>
         <frc-sendable-chooser source-key="/SmartDashboard/Auto Mode"></frc-sendable-chooser>
         <frc-basic-fms-info source-key="/FMSInfo"></frc-basic-fms-info>
         <frc-toggle-button label="Toggle Button" source-key="/Dashboard/Toggled"></frc-toggle-button>
-        <!-- <MyElement count="{$count}" onIncrement="{(value) => count.setValue(value)}" /> -->
       </div>
       <frc-field crop-left=".1" crop-right=".9" rotation-unit="deg">
         <frc-field-robot
