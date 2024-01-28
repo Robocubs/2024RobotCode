908915cc7ece2e2717032e7f6aa898e6cd72d1fc
Marc Pulte <marcpulte@gmail.com>
2024-01-28 00:31:19 -0500

    Added dashboard build to gradle

--- src/main/ts/dashboard/vite.config.ts
+++ src/main/ts/dashboard/vite.config.ts
@@ -1,3 +1,15 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    outDir: process.env.DASHBOARD_OUT_DIR ?? 'dist',
+    chunkSizeWarningLimit: 3000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          fwc: ['@frc-web-components/fwc'],
+        },
+      },
+    },
+  },
+});
