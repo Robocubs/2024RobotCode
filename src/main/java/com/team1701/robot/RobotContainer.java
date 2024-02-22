631bfbdbe098f6a810a1a0b1da8f37cdfc11c9df
Daniel Burke <danielburke1206@gmail.com>
2024-02-22 12:16:22 -0500

    fixed zeromethods

--- src/main/java/com/team1701/robot/RobotContainer.java
+++ src/main/java/com/team1701/robot/RobotContainer.java
@@ -425,8 +425,8 @@ public class RobotContainer {
     public Command getZeroCommand() {
         return runOnce(() -> {
                     // TODO: uncomment line
-                    // mShooter.zeroShooterRotation();
-                    // mArm.zeroArmRotation();
+                    mShooter.zeroShooterRotation();
+                    mArm.zeroArmRotation();
                     mDrive.zeroModules();
                 })
                 .ignoringDisable(true)
