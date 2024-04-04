7e4e1608b49765d1d3ba42aa9a12a3950d1919aa
Marc Pulte <marcpulte@gmail.com>
2024-04-04 01:36:07 -0400

    Corrected direction of orient modules in autonomous

--- src/main/java/com/team1701/robot/commands/PauseDrive.java
+++ src/main/java/com/team1701/robot/commands/PauseDrive.java
@@ -2,29 +2,37 @@ package com.team1701.robot.commands;
 
 import java.util.function.Supplier;
 
+import com.team1701.lib.util.GeometryUtil;
 import com.team1701.robot.Constants;
 import com.team1701.robot.states.RobotState;
 import com.team1701.robot.subsystems.drive.Drive;
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import org.littletonrobotics.junction.Logger;
 
 public class PauseDrive extends Command {
     private static final String kLoggingPrefix = "Command/PauseDrive/";
     private final Drive mDrive;
     private final RobotState mRobotState;
-    private Supplier<Pose2d> mTargetPoseSupplier;
-    private Command mRotationCommand;
+    private final Supplier<Rotation2d> mTargetModuleHeadingSupplier;
+    private final Command mRotationCommand;
 
-    public PauseDrive(Drive drive, RobotState robotState, Supplier<Pose2d> targetPoseSupplier) {
+    private Rotation2d mTargetModuleHeading = GeometryUtil.kRotationIdentity;
+
+    public PauseDrive(
+            Drive drive,
+            RobotState robotState,
+            Supplier<Rotation2d> targetModuleHeadingSupplier,
+            Supplier<Rotation2d> targetRobotHeadingSupplier) {
         mDrive = drive;
         mRobotState = robotState;
-        mTargetPoseSupplier = targetPoseSupplier;
+        mTargetModuleHeadingSupplier = targetModuleHeadingSupplier;
         mRotationCommand = new RotateToFieldHeading(
                 drive,
-                mTargetPoseSupplier.get()::getRotation,
+                targetRobotHeadingSupplier::get,
                 mRobotState::getHeading,
                 mRobotState::getToleranceSpeakerHeading,
                 Constants.Drive.kFastTrapezoidalKinematicLimits,
@@ -37,32 +45,28 @@ public class PauseDrive extends Command {
     @Override
     public void initialize() {
         mRotationCommand.initialize();
+        mTargetModuleHeading = mTargetModuleHeadingSupplier.get();
+        Logger.recordOutput(kLoggingPrefix + "TargetModuleHeading", mTargetModuleHeading);
     }
 
     @Override
     public void execute() {
-        if (Math.hypot(
-                        mDrive.getFieldRelativeVelocity().vxMetersPerSecond,
-                        mDrive.getFieldRelativeVelocity().vyMetersPerSecond)
-                < 0.5) {
-
-            mDrive.orientModules(mTargetPoseSupplier
-                    .get()
-                    .getTranslation()
-                    .minus(mRobotState.getPose2d().getTranslation())
-                    .getAngle());
+        var velocity = mDrive.getVelocity();
+        if (Math.hypot(velocity.vxMetersPerSecond, velocity.vyMetersPerSecond) < 0.5) {
+            mDrive.orientModules(mTargetModuleHeading.minus(mRobotState.getHeading()));
         } else {
             mRotationCommand.execute();
         }
     }
 
     @Override
+    public void end(boolean interrupted) {
+        Logger.recordOutput(kLoggingPrefix + "TargetModuleHeading", GeometryUtil.kRotationIdentity);
+    }
+
+    @Override
     public boolean isFinished() {
-        return MathUtil.isNear(
-                0,
-                Math.hypot(
-                        mDrive.getFieldRelativeVelocity().vxMetersPerSecond,
-                        mDrive.getFieldRelativeVelocity().vyMetersPerSecond),
-                0.1);
+        var velocity = mDrive.getVelocity();
+        return MathUtil.isNear(0, Math.hypot(velocity.vxMetersPerSecond, velocity.vyMetersPerSecond), 0.1);
     }
 }
