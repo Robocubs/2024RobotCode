c01a3d7920db96ec38e5e9fc9f5a719296d18105
faeqk <120421710+faeqk@users.noreply.github.com>
2024-06-29 08:51:56 -0400

    changed amp path

--- src/main/java/com/team1701/robot/FieldConstants.java
+++ src/main/java/com/team1701/robot/FieldConstants.java
@@ -62,7 +62,7 @@ public final class FieldConstants {
     public static final Translation2d kDroppedSourceNoteRedTranslation =
             new Translation2d(kFieldLongLengthMeters - 2.64, kFieldShortLengthMeters - 1.22);
 
-    public static final Translation2d kDroppedAmpNoteBlueTranslation = new Translation2d(2.03, 7.87);
+    public static final Translation2d kDroppedAmpNoteBlueTranslation = new Translation2d(1.97, 7.87);
     public static final Translation2d kDroppedAmpNoteRedTranslation =
-            new Translation2d(kFieldLongLengthMeters - 2.03, kFieldShortLengthMeters - 7.87);
+            new Translation2d(kFieldLongLengthMeters - 1.97, kFieldShortLengthMeters - 7.87);
 }
